e0e9c14ddbdee4d33189e6c0dc620d1235eb1b10
bhavyatyagi <bhavyatyagi16@gmail.com>
2022-04-27 10:39:57 +0530

    laying foundation of cg project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// ----------------------------- HEADER FILES STARTS ----------------------------------
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <math.h>
+#include <GLUT/glut.h>
+using namespace std;
+
+// ----------------------------- HEADER FILES ENDS ------------------------------------
+
+// ----------------------------- HELPER FUNCTIONS -------------------------------------
+
+// ----------------------------- MAIN PROGRAM STARTS ----------------------------------
+
+// ----------------------------- MAIN PROGRAM ENDS ------------------------------------
